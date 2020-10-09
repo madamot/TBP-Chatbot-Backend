6078fdd4235dfc06b59e590ecefd0d57ad302bcf
Madamot <adam@madamot.com>
2020-10-09 16:27:55 +0100

    chat api post and a simple response

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 const express = require('express');
+const uuid = require('uuid');
+const moment = require('moment');
 var cors = require('cors')
-const path = require('path');
 const chat = require('./Chat')
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Get Entire chat
 app.get('/api/chat', (req, res) => {
   res.json(chat);
 });
 
-// app.use(express.static(path.join(__dirname, 'public')));
+// Create Message
+app.post('/api/chat', (req, res) => {
+  const newMessage = {
+    id: uuid.v4(),
+    type: 'text',
+    title: req.body.title,
+    author: 'user',
+    date: moment().format(),
+  }
+
+  if(!newMessage.title) {
+    return res.status(400).json({ msg: 'No text in the message sent' })
+  }
+
+  chat.push(newMessage);
+  res.json({
+    id: uuid.v4(),
+    type: 'text',
+    title: 'We hear you',
+    author: 'bot',
+    date: moment().format(),
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
